cbe2b00d90fd4df04d81866f49b880b3cdfcc481
Wojtek Okoński <wojteko22.96@gmail.com>
2017-04-26 20:47:22 +0200

    Add files with task

---
+++ predict.py
@@ -0,0 +1,20 @@
+# --------------------------------------------------------------------------
+# ------------  Metody Systemowe i Decyzyjne w Informatyce  ----------------
+# --------------------------------------------------------------------------
+#  Zadanie 4: Zadanie zaliczeniowe
+#  autorzy: A. Gonczarek, J. Kaczmar, S. Zareba
+#  2017
+# --------------------------------------------------------------------------
+
+import pickle as pkl
+import numpy as np
+
+
+def predict(x):
+    """
+    Funkcja pobiera macierz przykladow zapisanych w macierzy X o wymiarach NxD i zwraca wektor y o wymiarach Nx1,
+    gdzie kazdy element jest z zakresu {0, ..., 35} i oznacza znak rozpoznany na danym przykladzie.
+    :param x: macierz o wymiarach NxD
+    :return: wektor o wymiarach Nx1
+    """
+    pass
