e8273f8ffc354868e8efde99bc1e11326687d28f
Wojtek Okoński <wojteko22.96@gmail.com>
2017-06-15 14:52:30 +0200

    Refactor

--- learning.py
+++ learning.py
@@ -8,19 +8,25 @@ def learn_model(x_train_features, y_train, x_val_features, y_val):
     classifier = MLPClassifier(solver='sgd', hidden_layer_sizes=(60, 50), random_state=1, max_iter=1,
                                # todo: zmienić warstwy
                                warm_start=True, batch_size=200)  # todo: zmienić batcha
-    for i in range(582):
+    record = 0
+    record_index = 0
+    for i in range(1, 5):
         classifier.fit(x_train_features, y_train)
         W, b, classes = classifier.coefs_, classifier.intercepts_, classifier.classes_
         result_y = p.predict_with_model_params(x_val_features, W, b, classes)
         reshaped_result_y = np.array(list(map(lambda yi: [yi], result_y)))
         result = u.check_prediction(reshaped_result_y, y_val)
-        print(str(i + 1) + ':\t', result)
+        if result > record:
+            record = result
+            record_index = i
+        print(i, ':\t', result)
         u.save_data(
             {
                 'W': W,
                 'b': b,
                 'classes': classes
-            }, 'models/' + str(i + 1) + '_' + str(result) + '.pkl')
+            }, 'models/' + str(i) + '_' + str(result) + '.pkl')
+    print(record_index, '=', record)
 
 
 data = u.read_file('plik.pkl')
