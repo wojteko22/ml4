6275631bd3d35b83b14da6e1bc2b87853faf0c13
Wojtek Okoński <wojteko22.96@gmail.com>
2017-06-15 11:27:46 +0200

    Refactor

--- predict.py
+++ predict.py
@@ -17,49 +17,41 @@ def predict(x):
     :param x: macierz o wymiarach NxD
     :return: wektor o wymiarach Nx1
     """
-
-    # for i in x:
-    #     y= hog(x.reshape(56,56))
-
-    def simple_predict(x):
-        x = prepare_one_dimen_array(x)
+    def predict_for_single(x):
         for i in range(len(W)):
-            x = W[i] @ x + b[i]
-            if i != len(coefs) - 1:
+            x = W[i].T @ x + b[i]
+            if i != len(W) - 1:
                 x = relu(x)
         return classes[np.argmax(x)]
 
-    x2 = get_hog_features(list(map(lambda i: i.reshape(56, 56), x)))
-    coefs, intercepts, classes = load_model('modelek2.pkl')
-    W = transpose_all_elements_in_fst_level_ndarray(coefs)
-    b = prepare_list_one_dimen_array(intercepts)
-    return np.array(list(map(lambda i: np.array([simple_predict(i)]), x2)))
+    reshaped_x = list(map(lambda xi: xi.reshape(56, 56), x))
+    hogged_x = list(map(lambda xi: hog(xi).flatten(), reshaped_x))
+    model = read_file('model.pkl')
+    W, b, classes = model['coefs'], model['intercepts'], model['classes']
+    return list(map(lambda xi: [predict_for_single(xi)], hogged_x))
 
 
-def get_hog_features(x):
-    return np.array(list(map(lambda y: hog(y).flatten(), x)))
+def relu(X):
+    np.clip(X, 0, np.finfo(X.dtype).max, out=X)
+    return X
 
 
 def hog(image):
     nwin_x = 5
-    nwin_y = 5  # podział obrazka
-    B = 7  # liczba kierunków
+    nwin_y = 5
+    B = 7
     (L, C) = np.shape(image)
     H = np.zeros(shape=(nwin_x * nwin_y * B, 1))
-    m = np.sqrt(L / 2.0)
     if C is 1:
         raise NotImplementedError
     step_x = np.floor(C / (nwin_x + 1))
     step_y = np.floor(L / (nwin_y + 1))
     cont = 0
-    hx = np.array([[1, 0, -1]])
-    hy = np.array([[-1], [0], [1]])
     hxy = np.array([1, 0, -1])
     grad_xr = np.convolve(image.flatten(), hxy, mode='same').reshape(56, 56)
     grad_yu = np.convolve(image.T.flatten(), hxy, mode='same').reshape(56, 56).T
     angles = np.arctan2(grad_yu, grad_xr)
     magnit = np.sqrt((grad_yu ** 2 + grad_xr ** 2))
-    print('\n')
     for n in range(nwin_y):
         for m in range(nwin_x):
             cont += 1
@@ -82,28 +74,6 @@ def hog(image):
     return H
 
 
-def transpose_all_elements_in_fst_level_ndarray(array):
-    return np.array(list(map(lambda x: x.transpose(), array)))
-
-
-def prepare_list_one_dimen_array(array):
-    return np.array(list(map(lambda x: prepare_one_dimen_array(x), array)))
-
-
-def prepare_one_dimen_array(x):
-    return np.array(list(map(lambda y: np.array([y]), x)))
-
-
-def relu(X):
-    np.clip(X, 0, np.finfo(X.dtype).max, out=X)
-    return X
-
-
-def load_model(file_name):
-    model = load_data(file_name)
-    return model['coefs'], model['intercepts'], model['classes']
-
-
-def load_data(file_name):
+def read_file(file_name):
     with open(file_name, 'rb') as f:
         return pkl.load(f)
--- run.py
+++ run.py
@@ -7,7 +7,20 @@ def celina():
     x = data[0][:50]
     y = data[1][:50]
     result = p.predict(x)
-    print(m.check_prediction(result, y))
+    print(check_prediction(result, y))
 
+def check_prediction(result_to_chech, y_true):
+    """
+
+    :param result_to_chech:
+    :param y_true:
+    :return:
+    """
+    error_count = 0
+    for i in range(50):
+        if result_to_chech[i][0] == y_true[i][0]:
+            error_count += 1
+
+    return error_count * 1.0 / 50
 
 celina()
