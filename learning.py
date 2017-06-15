d8dd270511bb77ef27973d625e3ea33a58d6a08f
Wojtek Okoński <wojteko22.96@gmail.com>
2017-06-15 19:08:11 +0200

    Refactor

--- learning.py
+++ learning.py
@@ -1,21 +1,17 @@
-import numpy as np
 import utils as u
 import predict as p
 from sklearn.neural_network import MLPClassifier
 
 
 def learn_model(x_train_features, y_train, x_val_features, y_val):
-    classifier = MLPClassifier(solver='sgd', hidden_layer_sizes=(45, 50), max_iter=2,
-                               # todo: zmienić warstwy
-                               warm_start=True, batch_size=50)
-    record = 0
+    classifier = MLPClassifier(hidden_layer_sizes=88, max_iter=6, warm_start=True, batch_size=40)   # 40 naj
+    record = 0.93
     record_index = 0
-    for i in range(1, 300):
+    for i in range(1, 100):
         classifier.fit(x_train_features, y_train)
         W, b, classes = classifier.coefs_, classifier.intercepts_, classifier.classes_
         result_y = p.predict_with_model_params(x_val_features, W, b, classes)
-        reshaped_result_y = np.array(list(map(lambda yi: [yi], result_y)))
-        result = u.check_prediction(reshaped_result_y, y_val)
+        result = u.check_prediction(result_y, y_val)
         if result > record:
             record = result
             record_index = i
@@ -27,10 +23,10 @@ def learn_model(x_train_features, y_train, x_val_features, y_val):
                 }, 'models/' + str(i) + '_' + str(result) + '.pkl')
             print(str(i) + ':\t', result)
         else:
-            print(i, end=" ")
+            print(result, end=" ", flush=True)
     print(record_index, '=', record)
 
 
 data = u.read_file('plik.pkl')
 parting = 22601
-learn_model(data[0][:parting], data[1][:parting], data[0][parting:], data[1][parting:])
+learn_model(data[0][:parting], data[1][:parting].ravel(), data[0][parting:], data[1][parting:])
